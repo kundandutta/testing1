9c84b33594c4c53f1c47eb64015578c3c2a73e25
kundan kumar dutta <duttakundan921@gmail.com>
2023-01-30 18:03:19 +0530

    updates js code for password & email

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ function formSubmitClick() {
     }
     console.log("Name ", name);
 
+    let password =document.getElementById("password").value;
+    if(password == ""){
+        alert("Please enter password");
+    }
+    console.log("Password", password);
+    
+    let mail = document.getElementById("mail").value;
+    if(mail == ""){
+        alert("Please Enter E-mail Id !");
+    }
+    console.log("Mail", mail)
+
+
 }
 
 
